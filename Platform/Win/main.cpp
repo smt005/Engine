21fee82835838fcc99f5e88c6c6fb2eee0c95b0d
Коптяев Александр <a.koptyaev@playrix.com>
2020-09-27 23:20:18 +0300

    Игра по умолчаниию задаётся в модуле Game

--- Platform/Win/main.cpp
+++ Platform/Win/main.cpp
@@ -5,6 +5,6 @@
 int main(int argc, char** argv)
 {
 	Engine::GamePtr gamePtr;
-	gamePtr = game::getGame(argc > 1 ? argv[1] : "MapExample");
+	gamePtr = game::getGame(argc > 1 ? argv[1] : std::string());
 	return Engine::Core::execution(gamePtr);
 }
