a40a38e487ff439baee4389e5addd9d0c194ecf6
Александр Коптяев <a.koptyaev@playrix.com>
2023-12-26 18:42:44 +0600

    size для Ortho_камеры по умолчанию 1.f

--- Source/Draw/Camera/Camera.h
+++ Source/Draw/Camera/Camera.h
@@ -83,7 +83,7 @@ public:
 	//...
 	void Init();
 	void SetPerspective(const float zFar = 1000000.f, const float zNear = 1.f, const float fov = 45.f);
-	void SetOrtho(const float size = 1000.f, const float zNear = -1000000.f, const float zFar = 1000000.f);
+	void SetOrtho(const float size = 1.f, const float zNear = -1000000.f, const float zFar = 1000000.f);
 	glm::vec3 corsorCoord();
 
 	void LookAt(const glm::vec3& pos, const glm::vec3& direct) {
