07939eb32366adf27e38d105d129e1f5012ca3c8
Александр Коптяев <a.koptyaev@playrix.com>
2023-12-29 19:16:24 +0600

    Возможность исключить PhysX из сборки.

--- Help/Script/Windows_vs17_2022_Win32.cmd
+++ Help/Script/Windows_vs17_2022_Win32.cmd
@@ -1,4 +1,4 @@
 @echo off
 mkdir "../../../../Build/Win32"
 cd "../../../../Build/Win32"
-cmake -DCMAKE_PLATFORM=Win32 -G "Visual Studio 17 2022" -A Win32 ../../Source
+cmake -DENABLE_PHYSIC=ON -G "Visual Studio 17 2022" -A Win32 ../../Source
--- Help/Script/Windows_vs17_2022_x64 disable Physic.cmd
+++ Help/Script/Windows_vs17_2022_x64 disable Physic.cmd
@@ -1,4 +1,4 @@
 @echo off
 mkdir "../../../../Build/Win_x64"
 cd "../../../../Build/Win_x64"
-cmake -DENABLE_PHYSIC=ON -G "Visual Studio 17 2022" -A x64 ../../Source
+cmake -DENABLE_PHYSIC=OFF -G "Visual Studio 17 2022" -A x64 ../../Source
